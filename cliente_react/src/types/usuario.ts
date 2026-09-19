8bd8cedd2c8a36c053e248b216e49157259ef994
DarkPhoenixSw <jorge.jimeneze777@gmail.com>
2026-09-19 16:18:17 -0300

    Revert "Subiendo proyecto"
    This reverts commit c8ef6dcd05a5d6811e505a40b209247f653052bc.

--- cliente_react/src/types/usuario.ts
+++
@@ -1,6 +0,0 @@
-import { email, nonEmpty, object, pipe, string} from "valibot";
-
-export const LoginFormSchema = object({
-    correo: pipe(string(), nonEmpty('Indique su email'), email('Correo no valido')),
-    password: pipe(string(), nonEmpty('Indique su contraseña'))
-})
